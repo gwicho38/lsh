ea4c2875bd9afe24b0445035d64cd7b734b0d2c5
lefv <lefv@c3.ai>
2023-11-30 21:35:21 -0500

    Added command for ishell.

--- src/app.ts
+++ src/app.ts
@@ -2,15 +2,17 @@ import { Command } from 'commander';
 import axios from 'axios';
 import { test } from './services/zapier.js'; // js extension because https://stackoverflow.com/questions/44979976/how-to-resolve-node-js-es6-esm-modules-with-the-typescript-compiler-tsc-tsc/70682797#70682797
 import { init_c3 } from './services/c3/c3.js'; // js extension because https://stackoverflow.com/questions/44979976/how-to-resolve-node-js-es6-esm-modules-with-the-typescript-compiler-tsc-tsc/70682797#70682797
+import { init_ishell } from './services/ishell/ishell.js';
 
 const program = new Command();
-
-init_c3(program);
 
 program
   .version('1.0.0')
   .description('lsh | shell utility for personal cli mgmt.')
   .name('lsh');
+
+init_c3(program);
+init_ishell(program);
 
 program
   .command('ran_i')
