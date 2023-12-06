3f998eb54fcaec9d1c5d4932a6a4aaf4180bebab
lefv <lefv@c3.ai>
2023-12-05 21:08:59 -0500

    Added genai entry point.

---
+++ src/lib/genai/genai.vs.ts
@@ -0,0 +1,15 @@
+const prepareMetadata = function (passageContent, sourceFile) {
+    sourceFile = sourceFile.getMissing({include: "metadata"});
+    if (sourceFile.metadata && sourceFile.metadata.tags) {
+        if (sourceFile.metadata.tags.length > 1) {
+            var continent = sourceFile.metadata.tags[0].id;
+            var country = sourceFile.metadata.tags[1].id;
+            return "The following passage is relevant for " + country + ", located in " + continent + ": " + passageContent
+        }
+    }
+    return passageContent
+ }
+
+const indexMetadataLambda = Lambda.fromJsFunc(prepareMetadata);
+const sc = Genai.SourceCollection.forId("default");
+sc.withField('indexedContentProvider', indexMetadataLambda).merge();
