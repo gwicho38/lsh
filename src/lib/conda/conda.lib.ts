3c247474e2ce35439eb9e250a6c3587b2d92db93
lefv <lefv@c3.ai>
2023-12-11 14:53:04 -0500

    Added c3 type files.

---
+++ src/lib/conda/conda.lib.ts
@@ -0,0 +1,24 @@
+import { makePOSTRequest } from "../../services/api/api.js";
+
+export async function get_conda_runtimes() {
+    // set c3fs
+    var origFSDefault = FileSystemConfig.make().getConfig().default;
+    FileSystemConfig.make().setDefault(FileSystemScheme.c3fs, ConfigOverride.TAG);
+    var origDefaultMount = FileSystem.mounts()['DEFAULT'];
+    var origExmfilesMount = FileSystem.mounts()['EXMFILES'];
+    
+    // prevent config from timing out
+    var config = CondaActionRuntimeConfig.make().getConfig();
+    config.setConfigValue("envInstallationTimeoutSeconds", 9999);
+    
+    // get all Python runtimes
+    var rts = TagMetadataStore.runtimes('Python');
+    // install runtimes
+    for (var rt in rts) {
+    try {
+        ActionRuntime.forName(rt).ensureInstalled();
+    } catch (e) {
+        console.log(e);
+    }
+    }
+}
