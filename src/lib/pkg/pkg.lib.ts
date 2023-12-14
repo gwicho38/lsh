4b79f2990797229c64048db34834fdec0f05091c
lefv <lefv@c3.ai>
2023-12-13 21:34:35 -0500

    removed old app.

--- src/lib/pkg/pkg.lib.ts
+++ src/lib/pkg/pkg.lib.ts
@@ -1,47 +1,47 @@
-// TODO: https://gkev8dev.c3dev.cloud/lefv20231117/gurusearchui/static/console/Pkg
-// TODO: https://gkev8dev.c3dev.cloud/lefv20231117/gurusearchui/static/console/Pkg.Store
+// // TODO: https://gkev8dev.c3dev.cloud/lefv20231117/gurusearchui/static/console/Pkg
+// // TODO: https://gkev8dev.c3dev.cloud/lefv20231117/gurusearchui/static/console/Pkg.Store
 
-import { ClusterStartSpec } from "@c3/types";
-import { lCluster } from "../types/lCluster.js";
-import { lClusterStartSpec } from "../types/lClusterStartSpec.js";
-import { c3_post } from "../../services/c3/c3.js";
-
-
-let clusterSpec: lClusterStartSpec  = {
-    withFailOnTimeout: function (failOnTimeout: boolean): ClusterStartSpec {
-        throw new Error("Function not implemented.");
-    },
-    maxRetries: 0,
-    withMaxRetries: function (maxRetries: number): ClusterStartSpec {
-        throw new Error("Function not implemented.");
-    },
-    initialSleepMillis: 0,
-    withInitialSleepMillis: function (initialSleepMillis: number): ClusterStartSpec {
-        throw new Error("Function not implemented.");
-    },
-    exponent: 0,
-    withExponent: function (exponent: number): ClusterStartSpec {
-        throw new Error("Function not implemented.");
-    },
-    withMaxSleepMillis: function (maxSleepMillis: number): ClusterStartSpec {
-        throw new Error("Function not implemented.");
-    },
-    withMaxTotalMillis: function (maxTotalMillis: number): ClusterStartSpec {
-        throw new Error("Function not implemented.");
-    },
-    withNodeCount: function (nodeCount: number): ClusterStartSpec {
-        throw new Error("Function not implemented.");
-    },
-    name: "lenv",
-    singleNode: true,
-    serverVersion: "8.3.0+197"
-}
+// import { ClusterStartSpec } from "@c3/types";
+// import { lCluster } from "../types/lCluster.js";
+// import { lClusterStartSpec } from "../types/lClusterStartSpec.js";
+// import { c3_post } from "../../services/c3/c3.js";
 
 
-export async function clearPkg(): Promise<void> {
+// let clusterSpec: lClusterStartSpec  = {
+//     withFailOnTimeout: function (failOnTimeout: boolean): ClusterStartSpec {
+//         throw new Error("Function not implemented.");
+//     },
+//     maxRetries: 0,
+//     withMaxRetries: function (maxRetries: number): ClusterStartSpec {
+//         throw new Error("Function not implemented.");
+//     },
+//     initialSleepMillis: 0,
+//     withInitialSleepMillis: function (initialSleepMillis: number): ClusterStartSpec {
+//         throw new Error("Function not implemented.");
+//     },
+//     exponent: 0,
+//     withExponent: function (exponent: number): ClusterStartSpec {
+//         throw new Error("Function not implemented.");
+//     },
+//     withMaxSleepMillis: function (maxSleepMillis: number): ClusterStartSpec {
+//         throw new Error("Function not implemented.");
+//     },
+//     withMaxTotalMillis: function (maxTotalMillis: number): ClusterStartSpec {
+//         throw new Error("Function not implemented.");
+//     },
+//     withNodeCount: function (nodeCount: number): ClusterStartSpec {
+//         throw new Error("Function not implemented.");
+//     },
+//     name: "lenv",
+//     singleNode: true,
+//     serverVersion: "8.3.0+197"
+// }
 
-}
 
-export async function bootstrapPkg(spec: lClusterStartSpec = clusterSpec): Promise<void> {
+// export async function clearPkg(): Promise<void> {
 
-}
+// }
+
+// export async function bootstrapPkg(spec: lClusterStartSpec = clusterSpec): Promise<void> {
+
+// }
