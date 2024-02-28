2c002da82df1e0a77415dbbc99da125dd53b3dbe
lefv <luis.fernandez-de-la-vara@c3.ai>
2024-02-28 17:29:57 -0500

    Added misc files.

---
+++ util/scripts/v8sync/config.js
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2009-2023 C3 (www.c3.ai). All Rights Reserved.
+ * This material, including without limitation any software, is the confidential trade secret and proprietary
+ * information of C3 and its licensors. Reproduction, use and/or distribution of this material in any form is
+ * strictly prohibited except as set forth in a written license agreement with C3 and/or its authorized distributors.
+ * This material may be covered by one or more patents or pending patent applications.
+ */
+
+module.exports = {
+    // Path to root repo. Exclude trailing slash
+    PATH_TO_PACKAGE_REPO: '/Users/lefv/test/20240102/prov/',
+  
+    // Listen to file updates within these packages
+    PACKAGES_TO_SYNC: ['guruSearch', 'guruSearchUI', 'govSecurityV8'],
+  
+    // Exclude trailing slash
+    APPURL: 'https://gkev8c3apps.c3-e.com/20240102v0/c3',
+  
+    // Basic auth token for BA:BA
+    // AUTH_TOKEN: 'Basic QkE6QkE=',
+    AUTH_TOKEN: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzUxMiJ9.eyJhcHAiOiJna2V2OGMzYXBwcy0yMDI0MDEwMnYwLWMzIiwiaXNzIjoiYzMuYWkiLCJncm91cHMiOlsiQzMuRW52QWRtaW4iXSwic2lkIjoxLCJhdWQiOiJjMy5haSIsImlkcCI6IiIsImMzZ3JvdXBzIjpbIkMzLkVudkFkbWluIl0sImlkcGdyb3VwcyI6IntcIk9pZGNJZHBDb25maWc6OmdrZXY4YzNhcHBzLmMzLWUuY29tXCI6W1wiZ2tldjhjM2FwcHMvU3R1ZGlvVXNlclwiXX0iLCJzc2lkeCI6IiIsIm5hbWUiOiIzMWNmNGZlZThjYWExYTZlMzAxYmQ3OTNkNTUzODUzYmM2NzYxM2RkY2Q5MjJiNWNkNTg1NzIwZDMyN2U2ZjEzIiwiYWN0aW9uaWQiOiI4OTM4LjI4Mzc1NzI2IiwiaWQiOiIzMWNmNGZlZThjYWExYTZlMzAxYmQ3OTNkNTUzODUzYmM2NzYxM2RkY2Q5MjJiNWNkNTg1NzIwZDMyN2U2ZjEzIiwiZXhwIjoxNzA0MzE0NzM4MDAwLCJlbWFpbCI6Imx1aXMuZmVybmFuZGV6LWRlLWxhLXZhcmFAYzMuYWkifQ.gPg1smRCSbZs-7xabOFJ70gTHKGhUxuoH9gcHSkQ4XRbOpaRIhbN9riXvW1iqvxrTBXpHdM_n9kdyuDtPymkGmPm5OjpWwvSm0DwxdbtBSwemiTP7U56_1suAwNJ3votRQ3aE86RvnR-Dyb74IK2xiE9jqNczyAtP7NNJQmcaetdecU5jRjb3AsxlAqE7nhddStcz-LDW990oqFqLRRjFExfcBo1OoVdhsnKlR7uWsw_SyFY9MDZ-35SZSg7RwnVljSc_zkj5StzVLEWq8k2TCYLcUlzy0TtdhOYwKyUygorzpY6S8h0PvjgJaTBtxOvK0ZQcB9sGlXARgk_Qmn9Rg'
+  };
+  
