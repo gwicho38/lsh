15a589043f31cd665bfac61d110d0e98a4b9dd14
lefv <luis@lefv.io>
2024-03-30 23:57:44 -0400

    Continued culling.

--- lsh/src/store/store.ts
+++ lsh/src/store/store.ts
@@ -11,32 +11,32 @@ export const setPrivateKey = (value: string) => { uKey = value; };
 
 // {"typ":"JWT","alg":"RS512"}
 let key = "EMPTY";
-export const getkey = () => key;
-export const setkey = (value: string) => { key = value; }
+export const getKey = () => key;
+export const setKey = (value: string) => { key = value; }
 
 // {"typ":"JWT","alg":"RS512"}
-export const getC3 = () => console.log("getC3");
-export const setC3 = (value: any) => console.log("setC3");
+export const getLSH = () => console.log("getLSH");
+export const setLSH = (value: any) => console.log("setLSH");
 
 export const get = () => console.log("hi");
 export const set = (value: any) => console.log(value);
 export const inst = (value: any) => console.log(value);
 export const kill = (value: any) => console.log(value);
 
-type LSH= {
+type LSH = {
     id?: number;
     name?: string;
     key?: string;
+    session?: string;
     pkgId?: string; 
     get?: any;
     set?: any;
 
 };
 
-export const c3: C3 = {};
+export const lsh: LSH = {};
 
 global.lsh = {
-    c3,
     get,
     set,
     inst,
