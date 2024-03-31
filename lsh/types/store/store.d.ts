6df2b20dd16672b9649b0b8475267db43ed49b63
lefv <luis@lefv.io>
2024-03-31 00:03:40 -0400

    Success with tsc

--- lsh/types/store/store.d.ts
+++ lsh/types/store/store.d.ts
@@ -2,22 +2,22 @@ export declare const getPkgId: () => string;
 export declare const setPkgId: (value: string) => void;
 export declare const getPrivateKey: () => string;
 export declare const setPrivateKey: (value: string) => void;
-export declare const getkey: () => string;
-export declare const setkey: (value: string) => void;
-export declare const getKey: () => void;
-export declare const setKey: (value: any) => void;
+export declare const getKey: () => string;
+export declare const setKey: (value: string) => void;
+export declare const getLSH: () => void;
+export declare const setLSH: (value: any) => void;
 export declare const get: () => void;
 export declare const set: (value: any) => void;
 export declare const inst: (value: any) => void;
 export declare const kill: (value: any) => void;
-type LSH= {
+type LSH = {
     id?: number;
     name?: string;
     key?: string;
+    session?: string;
     pkgId?: string;
     get?: any;
     set?: any;
 };
 export declare const lsh: LSH;
-export { };
-
+export {};
