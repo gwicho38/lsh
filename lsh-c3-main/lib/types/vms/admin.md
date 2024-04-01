acc3bf61b973f8bd3d8ebe368200759570593e1c
Luis <luis@lefv.io>
2024-04-01 13:29:17 -0400

    Removed unnecessary files from repo.

--- lsh-c3-main/lib/types/vms/admin.md
+++
@@ -1,5 +0,0 @@
-```bash
-# ubuntu vm access
-ssh c3admin@137.135.12.235
-# user acct -- lefv
-```
--- lsh-c3-main/lib/ui/studio.lib.ts
+++
@@ -1,9 +0,0 @@
-// https://community.c3.ai/t/how-to-increase-env-limit-for-a-specific-user/13401
-
-// Thanks @c3-sethtilliss for sending over this command:
-
-// StudioUserConfig.forId(id).setConfigValue('maxSingleNodeEnvPerStudioUser', 2)
-
-// And for MNE:
-
-// StudioUserConfig.forId(id).setConfigValue('maxNonSingleNodeEnvPerStudioUser', 2)
