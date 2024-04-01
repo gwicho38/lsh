7992d8593df7382299a9bb8c9b0e4a9ba5714056
lefv <luis@lefv.io>
2024-04-01 01:01:03 -0400

    Handled io buffering.

--- lsh/src/components/UserInput.tsx
+++ lsh/src/components/UserInput.tsx
@@ -1,15 +1,52 @@
-import { Box } from 'ink';
-import React from 'react';
-import { Divider } from './Divider.js';
+import { useInput, useStdout } from 'ink';
+import React, { useState } from 'react';
+
+type Props = {
+	name: string | undefined;
+};
 
 export function UserInput({ ...props }) {
+	const [feature, setFeature] = useState('');
+	const [readData, setReadData] = useState([]);
+	const [writeData, setWriteData] = useState([]);
+	const [userInput, setUserInput] = useState('');
+	const [name, setName] = useState('');
+	const handleSubmit = query => {
+		// Do something with query
+		console.log(query);
+		setUserInput('');
+
+	};
+	const {stdout, write} = useStdout();
+
+	const handleChange = query => {
+	};
+
+	useInput((input, key) => {
+		console.log(input);
+		// console.log(key);
+		if (input === 'q') {
+			// Exit program
+		};
+	});
+
 	const input = (
 		<>
-			<Divider />
-			<Box flexDirection="column">
+			{/* <Divider /> */}
 
-			</Box>
-			<Divider />
+			{/* <UncontrolledTextInput
+				focus={true}
+				onSubmit={handleSubmit}
+				initialValue={userInput}
+				value={userInput}
+				onChange={handleChange}
+				placeholder={''}
+				// mask={'*'}
+				highlightPastedText={true}
+				showCursor={true}>
+					{stdout.value}
+			</UncontrolledTextInput> */}
+			{/* <Divider /> */}
 		</>
 	);
 	return input;
