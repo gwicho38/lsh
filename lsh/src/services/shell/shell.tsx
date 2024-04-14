b4b1993e93255c8dbd8b8db142205d2924252d2c
lefv <luis@lefv.io>
2024-04-13 23:22:21 -0400

    Added dynamic imports.

--- lsh/src/services/shell/shell.tsx
+++ lsh/src/services/shell/shell.tsx
@@ -1,15 +1,16 @@
-import { Command } from 'commander';
-import { render } from 'ink';
-import React from 'react';
-import { UserInput } from '../../components/UserInput.js';
+import { Command } from "commander";
+import { render } from "ink";
+import React from "react";
+import { UserInput } from "../../components/UserInput.js";
 
 interface Spec {
-  function: String
+  function: String;
 }
 
-export async function get_ishell(type: String = "Jvm", spec: Spec = { function: "currentUsage" }) {
-
-}
+export async function get_ishell(
+  type: String = "Jvm",
+  spec: Spec = { function: "currentUsage" }
+) {}
 
 export async function init_ishell(program: Command) {
   cmd_interactive(program);
@@ -17,11 +18,9 @@ export async function init_ishell(program: Command) {
 
 async function cmd_interactive(program: Command) {
   program
-    .command('repl')
-    .description('lsh interactive shell')
+    .command("repl")
+    .description("lsh interactive shell")
     .action(async (type: String, action: String, spec: Spec) => {
-
-      render(<UserInput/>);
-
+      render(<UserInput />);
     });
 }
