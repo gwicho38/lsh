e99da5ba3f69d6d8ae2fbf37202faa0fc17b42f4
lefv <luis@lefv.io>
2024-04-13 21:56:01 -0400

    Reading command logic.

--- lsh/src/services/lib/lib.ts
+++ lsh/src/services/lib/lib.ts
@@ -1,12 +1,33 @@
 import { Command } from "commander";
-import { loadCommands } from "../../util/lib.util.js";
+import { CommandDictionary, getFiles } from "../../util/lib.util.js";
+
+async function getCommands(files: any) {
+  console.log("getCommands");
+  files.forEach(async (file: any) => {
+    if (file !== "lib.ts") {
+      const cmd_exports = await import(`./${file.split(".")[0]}.js`);
+      for (const [key, value] of Object.entries(cmd_exports)) {
+        if (key.indexOf("cmd") !== -1) {
+          console.log(key.split("cmd_"));
+          console.log(value);
+        } 
+      }
+    }
+  });
+}
+
+export async function loadCommands() {
+  console.log("loadCommands");
+  const files = await getFiles();
+  getCommands(files);
+  const commands: CommandDictionary = {};
+}
 
 export async function init_lib_cmd(program: Command) {
   program
     .command("lib <cmd> [...options]")
     .description("lsh lib commands")
     .action(async (type: String, action: String, spec: Spec) => {
-        const commands = await loadCommands();
-        console.log(commands);
+      const commands = await loadCommands();
     });
 }
--- lsh/types/services/lib/lib.d.ts
+++ lsh/types/services/lib/lib.d.ts
@@ -1,2 +1,3 @@
 import { Command } from "commander";
+export declare function loadCommands(): Promise<void>;
 export declare function init_lib_cmd(program: Command): Promise<void>;
