358b472b3e55a989a77f6a8cd5a05dbd48568cc2
lefv <luis.fernandez-de-la-vara@c3.ai>
2024-04-17 08:29:52 -0700

    Added global code.

--- lpy/src/lpy/app/main.py
+++ lpy/src/lpy/app/main.py
@@ -1,23 +1,27 @@
+import typer
+import IPython
 from lpy.c3 import load_c3
 
-# def main() -> int:
-#     c3 = load_c3(url=url, auth=TOKEN)
-#     print(c3)
+c3 = {}
+app = {}
+
+def set_global(global_name: str, global_value: any):
+    global global_name
+    global_name = global_value
+
+def get_global(global_name: str):
+    global global_name
+    return global_name
+
 
 TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzUxMiJ9.eyJhcHAiOiJsb2NhbC1jMy1jMyIsImZpcnN0bmFtZSI6IkJBX0ZpcnN0IiwiaXNzIjoiYzMuYWkiLCJncm91cHMiOlsiQzMuQ2x1c3RlckFkbWluIl0sImxhc3RuYW1lIjoiQkFfTGFzdCIsInNpZCI6MSwiYXVkIjoiYzMuYWkiLCJpZHAiOiJUZXN0SWRwIiwiYzNncm91cHMiOlsiQzMuQ2x1c3RlckFkbWluIl0sImlkcGdyb3VwcyI6Int9Iiwic3NpZHgiOiIiLCJuYW1lIjoiQkEiLCJhY3Rpb25pZCI6Ijk2NzQuMjAzOTk1IiwiaWQiOiJCQSIsImV4cCI6MTcxMzQxNzA2MTAwMCwiZW1haWwiOiJiYUBjMy5haSJ9.K1b0D5lLg4hptzkf_Gi2aPuwL9I6FOIGjtdO_kb9sSxP2GHCv3gE1GNkwi4OK90Ia3GAMXXzq8C_naSjEk0CAatCsdDCZ-qY4kKwzkOEuEJGqMOYs4UcCPk_AvndX8fX6eJxdne0s6A41TbWkZhCXLrZMtmhko1U1IlrfK9a01fz8ytoooEc5o2tq9paBdDycuuJGTdaFkuP94DStohnyduHjNkcJnLt1RWlhFgBH49V-DzwL5JelKIar-vJ2b2NsPPJ1V9Vawp6_akCAXUWhwkpkYiQQOZKfigWacUdtZlLdeCc_gA86yoXi9xo63x5ZARyqRYMZGWave4bN2YUTg"
 
 url = "http://localhost:8888/c3/c3"
 
 def main() -> None:
-    import IPython
-
-    c3 = load_c3(url, TOKEN)
-    print(c3)
-    print("lpy/main.py")
-    # lots of code
-    # even more code
+    set_global("app", typer.Typer())
+    set_global("c3", load_c3(url, TOKEN))
     IPython.embed()
 
 if __name__ == "__main__":
-    # c3 = load_c3(url=url, auth=TOKEN)
     print("main.py")
