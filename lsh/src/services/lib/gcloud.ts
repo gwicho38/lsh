49dd894b1e9c68b3bb5e6ff4454ef811e7b9737b
lefv <luis.fernandez-de-la-vara@c3.ai>
2024-04-17 15:36:05 -0700

    Added async spawn logic in lieu of exec

--- lsh/src/services/lib/gcloud.ts
+++ lsh/src/services/lib/gcloud.ts
@@ -1,12 +1,77 @@
-import { exec } from "child_process";
+import { spawn } from "child_process";
 
 export async function cmd_gcp_auth() {
-  exec("gcloud auth login", (err, stdout, stderr) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-
-    console.log(stdout);
+  const cmd = spawn(process.env.CMD_GCP_LOGIN);
+  cmd.stdout.on("data", (data) => {
+    console.log(`stdout: ${data}`);
   });
+
+  cmd.stderr.on("data", (data) => {
+    console.error(`stderr: ${data}`);
+  });
+
+  cmd.on("close", (code) => {
+    console.log(`child process exited with code ${code}`);
+  });
+}
+
+export async function cmd_gcp_start() {
+  const cmd = spawn(process.env.CMD_GCP_START);
+  cmd.stdout.on("data", (data) => {
+    console.log(`stdout: ${data}`);
+  });
+
+  cmd.stderr.on("data", (data) => {
+    console.error(`stderr: ${data}`);
+  });
+
+  cmd.on("close", (code) => {
+    console.log(`child process exited with code ${code}`);
+  });
+}
+
+export async function cmd_gcp_status() {
+  const cmd = spawn(process.env.CMD_GCP_STATUS);
+  cmd.stdout.on("data", (data) => {
+    console.log(`stdout: ${data}`);
+  });
+
+  cmd.stderr.on("data", (data) => {
+    console.error(`stderr: ${data}`);
+  });
+
+  cmd.on("close", (code) => {
+    console.log(`child process exited with code ${code}`);
+  });
+}
+
+export async function cmd_gcp_tunnel() {
+  const cmd = spawn(process.env.CMD_GCP_TUNNEL);
+  cmd.stdout.on("data", (data) => {
+    console.log(`stdout: ${data}`);
+  });
+
+  cmd.stderr.on("data", (data) => {
+    console.error(`stderr: ${data}`);
+  });
+
+  cmd.on("close", (code) => {
+    console.log(`child process exited with code ${code}`);
+  });
+}
+
+export async function cmd_gcp_ps() {
+  const cmd = spawn(process.env.CMD_GCP_PS);
+
+    cmd.stdout.on("data", (data) => {
+      console.log(`stdout: ${data}`);
+    });
+  
+    cmd.stderr.on("data", (data) => {
+      console.error(`stderr: ${data}`);
+    });
+  
+    cmd.on("close", (code) => {
+      console.log(`child process exited with code ${code}`);
+    });
 }
