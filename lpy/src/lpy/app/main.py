980323ea4f8f478b8475ee1b81dc780a199223bb
lefv <luis.fernandez-de-la-vara@c3.ai>
2024-04-17 07:23:15 -0700

    import statements work.

--- lpy/src/lpy/app/main.py
+++ lpy/src/lpy/app/main.py
@@ -1,8 +1,16 @@
-from lpy.c3 import c3
+from lpy.c3 import load_c3
 
-def main() -> int:
-    print("lpy")
+# def main() -> int:
+#     c3 = load_c3(url=url, auth=TOKEN)
+#     print(c3)
+
+TOKEN = "REDACTED"
+
+url = "http://localhost:8888/c3/c3"
+
+def main() -> None:
+    print("lpy/main.py")
 
 if __name__ == "__main__":
-    main()
-    c3()
+    # c3 = load_c3(url=url, auth=TOKEN)
+    print("main.py")
