970711668e9c87959e0d2df8b40b8e351283b64e
lefv <luis.fernandez-de-la-vara@c3.ai>
2024-04-17 15:36:05 -0700

    Added async spawn logic in lieu of exec

--- lsh/types/services/lib/gcloud.d.ts
+++ lsh/types/services/lib/gcloud.d.ts
@@ -1,1 +1,5 @@
 export declare function cmd_gcp_auth(): Promise<void>;
+export declare function cmd_gcp_start(): Promise<void>;
+export declare function cmd_gcp_status(): Promise<void>;
+export declare function cmd_gcp_tunnel(): Promise<void>;
+export declare function cmd_gcp_ps(): Promise<void>;
