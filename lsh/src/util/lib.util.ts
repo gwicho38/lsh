42db84fb7240a322a77b31e2e25e4fe1f9efc4b7
lefv <luis.fernandez-de-la-vara@c3.ai>
2024-04-18 19:34:47 -0700

    moved util functions to utils.

--- lsh/src/util/lib.util.ts
+++ lsh/src/util/lib.util.ts
@@ -1,5 +1,6 @@
 import * as fs from "fs";
 import { dirname } from "path";
+import { stdout } from "process";
 import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
@@ -26,7 +27,29 @@ export interface CommandDictionary {
   [filename: string]: Function[];
 }
 
-export async function getFiles() {
-  return await fs.promises.readdir("./src/services/lib/");
+export async function getFiles(path: string) {
+  return await fs.promises.readdir(path);
 }
 
+export async function loadCommands(baseCmd: string, path: string) {
+  const files = await getFiles(path);
+  const cmdMap = await parseCommands(baseCmd, files, path);
+  return cmdMap;
+}
+
+async function parseCommands(baseCmd: string, files: any, path: string) {
+  let commands = {};
+  for (const file of files) {
+    if (file !== baseCmd) {
+      // TODO: Generalize further
+      const cmd_exports = await import(`../services/${baseCmd.split(".")[0]}/${file.split(".")[0]}.js`);
+      for (const [key, value] of Object.entries(cmd_exports)) {
+        if (key.indexOf("cmd") !== -1) {
+          commands[key.split("cmd_")[1]] = value;
+        }
+      }
+    }
+  }
+
+  return commands;
+}
--- lsh/types/services/lib/gcloud.d.ts
+++ lsh/types/services/lib/gcloud.d.ts
@@ -1,3 +1,15 @@
+/**
+ * gcloud workstations start \
+  --project=remote-ws-88 \
+  --cluster=cluster-cirrus-ws \
+  --config=config-cirrus-ws \
+  --region=us-west1 \
+  apps-luis-fernandez-de-la-vara
+Starting workstation: [apps-luis-fernandez-de-la-vara]
+Waiting for operation [projects/remote-ws-88/locations/us-west1/operations/operati
+on-1713394821232-61652d1adf7ef-892433ff-4479a5c2] to complete...done.
+Started workstation [apps-luis-fernandez-de-la-vara].
+ */
 export declare function cmd_gcp_auth(): Promise<void>;
 export declare function cmd_gcp_start(): Promise<void>;
 export declare function cmd_gcp_status(): Promise<void>;
