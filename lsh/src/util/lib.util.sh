04aec3eba6e880869ec1d88aae5294d648915050
lefv <luis.fernandez-de-la-vara@c3.ai>
2024-05-14 15:30:16 -0400

    Adding search functionality.

--- lsh/src/util/lib.util.sh
+++
@@ -1,1 +0,0 @@
-tree -P "*.c3typ" --prune -f -I 'node_modules|govSecurityV8|linting' | grep Service
--- lsh/src/util/lib.util.ts
+++ lsh/src/util/lib.util.ts
@@ -2,6 +2,9 @@ import * as fs from "fs";
 import { dirname } from "path";
 import { stdout } from "process";
 import { fileURLToPath } from "url";
+import path from "path";
+import Fuse from "fuse.js";
+import ignore from "ignore";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -50,9 +53,42 @@ async function parseCommands(baseCmd: string, files: any, path: string) {
           commands[key.split("cmd_")[1]] = value;
         }
       }
-      const cmd_shell_exports = await console.log("hi");
+      // const cmd_shell_exports = await console.log("hi");
     }
   }
 
   return commands;
 }
+
+// Function to read and parse .gitignore
+const parseGitignore = (baseDir) => {
+  const ig = ignore();
+  const gitignorePath = path.join(baseDir, ".gitignore");
+
+  if (fs.existsSync(gitignorePath)) {
+    const gitignoreContent = fs.readFileSync(gitignorePath, "utf8");
+    ig.add(gitignoreContent);
+  }
+
+  return ig;
+};
+
+// Function to recursively get all TypeScript files in a directory excluding "d.ts", "dist" directory, and ".gitignore" patterns
+const getAllTsFiles = (dir, ig, fileList = []) => {
+  const files = fs.readdirSync(dir);
+  files.forEach((file) => {
+    const filePath = path.join(dir, file);
+    const relativeFilePath = path.relative(process.cwd(), filePath);
+
+    if (ig.ignores(relativeFilePath) || relativeFilePath.startsWith("dist")) {
+      return;
+    }
+
+    if (fs.statSync(filePath).isDirectory()) {
+      getAllTsFiles(filePath, ig, fileList);
+    } else if (filePath.endsWith(".ts") && !filePath.endsWith("d.ts")) {
+      fileList.push(filePath);
+    }
+  });
+  return fileList;
+};
