c55fd9a86cb0976d894b74693bd9366256880da0
lefv <luis.fernandez-de-la-vara@c3.ai>
2024-09-02 16:42:56 -0400

    Adding types.

---
+++ lsh/src/declarations/CoLBERT.ts
@@ -0,0 +1,5 @@
+import { IDataConfig as DataConfig } from "./DataConfig";
+import { IObject as Object } from "./Object";
+export interface ICoLBERT extends Object {
+  DataConfig: DataConfig;
+}
---
+++ lsh/src/declarations/UniversalChunker.d.ts
@@ -0,0 +1,5 @@
+import { IConfig as Config } from "./Config";
+
+export interface IUniversalChunker {
+  Config: Config;
+}
